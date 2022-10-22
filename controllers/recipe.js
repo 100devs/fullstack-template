49aee968b691ef638bf058017e608fa9d712ddf6
Antony Ekirapa <85803523+NueMe@users.noreply.github.com>
2022-10-22 03:49:44 +0300

    changed favicon

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -8,9 +8,9 @@ module.exports = {
       //Since we have a session each request (req) contains the logged-in users info: req.user
       //console.log(req.user) to see everything
       //Grabbing just the posts of the logged-in user
-      const posts = await Recipe.find({ user: req.user.id });
+      const recipes = await Recipe.find({ user: req.user.id });
       //Sending post data from mongodb and user data to ejs template
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render("profile.ejs", { recipes: recipes, user: req.user });
     } catch (err) {
       console.log(err);
     }
